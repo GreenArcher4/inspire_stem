c162bd6594cb023df64b38f974f845ccab407b77
GreenArcher <lornawaithanji@gmail.com>
2024-02-23 12:15:17 +0300

    pascals_tri

--- week_2/pascals_tri.py
+++ week_2/pascals_tri.py
@@ -5,7 +5,7 @@
 from math import factorial 
 
 col= int(input("Enter the number of columns in the pattern "))
-print (" =====tha pattern is====")
+print (" =====tha pattern is====") #indicates the beggining of the pattern
 
 for i in range(0, col):
     for v in range (col -i +1):
@@ -14,5 +14,5 @@ for i in range(0, col):
     for v in range (0, i + 1):
           # representing pascals formula: nCr= n!/((n-r)!r!)
      print( factorial(i)//(factorial(v)* factorial(i-v)), end =  "   " )
- 
+
 print( )
