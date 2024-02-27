4cdb3c634ca3810f29ed734123b20cec49cfefa3
GreenArcher <lornawaithanji@gmail.com>
2024-02-27 15:39:42 +0300

    salary

--- week_3/salary.py
+++ week_3/salary.py
@@ -2,24 +2,19 @@
 # Date: 26/02/2024
 # Name : Lorna 
 
-name = 'MiChael'
-salary = 140000
+name = str(input("Enter your name:"))
+salary = int(input("Enter your salary:"))
+print("\n")                                 #skip a line
 
-if salary <100000 :
- 
+if salary < 100000 :
  new_salary = salary *1.3
- print(name)
- print("raise of 30%:", new_salary)
+ print(name,",your new salary is:", new_salary,' enjoy ;)')
 
 elif salary< 150000 :
- 
  new_salary = salary *1.15
- print(name)
- print("raise of 15 %:", new_salary)
+ print(name,",your new salary is:", new_salary , ' enjoy ;)')
 
 else :
- 
  salary>150000 
  new_salary = salary *1.05
- print(name)
- print("raise of 5 %:", new_salary)
+ print(name,",your new salary is:", new_salary, ' enjoy ;)')
