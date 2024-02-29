f769d55bf38d507b3b025b5fa55bde5cbaf79911
GreenArcher <lornawaithanji@gmail.com>
2024-02-29 09:54:22 +0300

    dictionaries, turple

---
+++ week_3/functions.py
@@ -0,0 +1,37 @@
+# All about functions(user defined)(built in)
+# Date: 29/02/2024
+# Name : Lorna 
+
+'''
+block of code running together as a unit
+Steps;
+-initialise (uses a key word:def)
+-Call the function
+'''
+
+#Define a function
+def print_name():
+    print("My name is Barca Waithan")
+
+#Calling the function
+print_name()
+
+#details
+def print_details(name,age,id,gender):
+    print("I am {0}, {1}years old. My id no is {2} and my gender is {3}".format(name,age, id, gender))
+
+print_details("Barca Waithan","19","090326","female")
+print_details("Vincent ","21","309124","male")
+
+#sum of numbers
+
+def sum_nums(x,y):
+    return x+y
+
+z = sum_nums(10,20)
+print(z)
+
+def prod_nums(x,y):
+    return x*y
+
+print(prod_nums(5,79))
--- week_3/turple_hobby.py
+++ week_3/turple_hobby.py
@@ -1,4 +1,4 @@
-# Turple of some hobbies and car
+# Turple of some hobbies 
 # Date: 28/02/2024
 # Name : Lorna 
 
@@ -12,26 +12,3 @@ for hobby in hobbies:
     print(hobby,"\n")
 
 
-#Favorite car
-car = {"model":"Bugatti Chiron","color":"sleek black","horsepower":"1500","year":"2021","top speed":"261mph","type":"luxury sports"}
-print(car,"\n")
-
-print(car["model"])
-print(car["color"])
-print(car["horsepower"])
-print(car["year"])
-print(car["top speed"])
-print(car["type"])
-
-eco_car ={"model":"Tesla s","color":"midnight blue ","battery":"100kWh","horsepower":"1020","top speed":"200mph","type":"Executive"}
-print(car,"\n")
-
-print(eco_car["model"])
-print(eco_car["color"])
-print(eco_car["battery"])
-print(eco_car["horsepower"])
-print(eco_car["top speed"])
-print(eco_car["type"])
-
-vroom = car.copy
-print(vroom)
